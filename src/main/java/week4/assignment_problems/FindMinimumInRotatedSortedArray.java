098765cc9c195ea25614b9b0c9690e64527e6f2e
abtcoderrmilli <thapaankur2000@gmail.com>
2026-09-06 14:35:15 +0530

    Add Session 4 class and assignment problems

---
+++ src/main/java/week4/assignment_problems/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,35 @@
+package main.java.week4.assignment_problems;
+
+public class FindMinimumInRotatedSortedArray {
+
+    public static int findMin(int[] nums) {
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+
+                left = mid + 1;
+
+            } else {
+
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+
+        System.out.println(
+                "Minimum Element: " + findMin(nums)
+        );
+    }
+}
